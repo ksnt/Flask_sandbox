2240c5e50340edcad4982ce5881125d6860a9466
Takayuki Kaisen <ksn0215@gmail.com>
2019-04-28 04:20:11 +0900

    add hello3.py

---
+++ hello3.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return redirect('http://www.example.com')
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
